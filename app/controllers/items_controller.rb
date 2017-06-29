9a62de079f335482f0f07d526f4440226046a619
Tara Briggs <tebriggs4@gmail.com>
2017-06-29 22:04:17 +0000

    mark-todo-items-complete complete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,6 +12,22 @@ class ItemsController < ApplicationController
     end
   end
   
+  def destroy
+    @item = Item.find(params[:id])
+    if @item.destroy
+      # flash.now[:notice] = "Item has been deleted."
+      # redirect_to user_path(current_user) - no longer needed since using AJAX
+    else
+      # flash.now[:alert] = "There was an issue deleting the item. Please try again."
+      # render :show - no longer needed since using AJAX
+    end
+    
+    respond_to do |f|
+      f.html
+      f.js
+    end
+  end
+  
   private
   def item_params
     params.require(:item).permit(:name)
---
+++ db/migrate/20170627144824_add_completed_to_item.rb
@@ -0,0 +1,5 @@
+class AddCompletedToItem < ActiveRecord::Migration
+  def change
+    add_column :items, :completed, :boolean, default: false
+  end
+end
